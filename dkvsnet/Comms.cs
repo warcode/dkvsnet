59ba4a9b911e2642ea75431973a70ef1e7fd404a
warcode <warcode@deny.io>
2014-10-22 23:11:19 +0200

    init

---
+++ dkvsnet/Comms.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZeroMQ;
+using System.Threading;
+using System.Reactive;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+
+namespace dkvsnet
+{
+    class Comms
+    {
+        public ZmqContext context;
+        private int listenport;
+        public BlockingCollection<RaftMessage> append;
+        private ConcurrentQueue<RaftMessage> queue;
+        public string LocalHost;
+
+
+        public Comms(int port)
+        {
+            listenport = port;
+            queue = new ConcurrentQueue<RaftMessage>();
+            append = new BlockingCollection<RaftMessage>(queue);
+        }
+
+        public void StartIncoming()
+        {
+            // ZMQ Context, server socket
+            context = ZmqContext.Create();
+            LocalHost = LocalIPAddress() +":"+ listenport;
+
+            Task.Factory.StartNew(() =>
+            {
+                string message;
+                try
+                {
+                    ZmqSocket server = context.CreateSocket(ZeroMQ.SocketType.REP);
+                    server.Bind("tcp://"+ LocalHost);
+
+                    while ((message = server.Receive(Encoding.Unicode)) != null)
+                    {
+                        Console.WriteLine("Received request: {0}", message);
+                        var dataParts = message.Split('|');
+                        if(dataParts.Count() < 2)
+                        {
+                            continue;
+                        }
+                        var type = dataParts[0];
+                        var sender = dataParts[1];
+
+                        var data = (dataParts.Count() > 2 ? dataParts[2] : "");
+
+                        var msg = new RaftMessage
+                        {
+                            Type = type,
+                            Sender = sender,
+                            Data = data
+                        };
+
+                        append.Add(msg);
+
+                        server.Send("TRUE", Encoding.UTF8);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Out.Write(ex);
+                }
+            }, TaskCreationOptions.LongRunning);
+        }
+
+        public string LocalIPAddress()
+        {
+            IPHostEntry host;
+            string localIP = "";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            return localIP;
+        }
+    }
+}
---
+++ dkvsnet/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Microsoft.Owin;
+using Owin;
+using ZeroMQ;
+using System.Threading;
+
+namespace dkvsnet
+{
+    class Program
+    {
+
+        static string value = "";
+        static ZmqContext context;
+        static string remote;
+
+
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:" + (Int32.Parse(args[0]) + 1000)))
+            {
+                var localport = args[0];
+
+                var c = new Comms(Int32.Parse(localport));
+
+                c.StartIncoming();
+
+                var raft = new Raft(c.context, c.LocalHost);
+
+                raft.Start(c.append);
+
+                while (true)
+                {
+                    raft.Timeout();
+                    Thread.Sleep(10);
+                }
+            }
+        }
+    }
+
+
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            #if DEBUG
+            app.UseErrorPage();
+            #endif
+            
+            app.UseWelcomePage("/");
+
+            app.Run(context =>
+            {
+                if (context.Request.Path == new PathString("/test"))
+                {
+                    return context.Response.WriteAsync("Hello, world");
+                }
+
+                if (context.Request.Path == new PathString("/get"))
+                {
+                    return context.Response.WriteAsync("get");
+                }
+
+                if (context.Request.Path == new PathString("/set"))
+                {
+                    return context.Response.WriteAsync("set");
+                }
+
+                return context.Response.WriteAsync("what");
+            });
+        }
+    }
+}
